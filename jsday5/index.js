27d98e46aeafed968f490dd1bf1ae6ca31df2cd5
AbhishekGupta0164 <abhishekgupta0164@gmail.com>
2026-05-12 21:34:42 +0530

    Calculator

---
+++ jsday5/index.js
@@ -0,0 +1,35 @@
+
+// let h1=  document.getElementsByClassName("two")
+// console.log(h1,(0).innerText);
+
+// let h1=  document.querySelector("h1")
+
+// let p= document.createElement("p")
+// p.innerText="hello"
+// h1.uppendchild(p)
+// console.log(h1)
+
+// h1.innerText="welcome to js day 5"
+
+// let name = prompt("Enter your name: ")
+// h3.userdate
+
+
+
+let inp= document.querySelector("input")
+let h2= document.querySelector("h2")
+let but= document.querySelector("button")
+
+btn.addEventListener("Click",Function(){
+console.log("clicked")
+h2.innerText=data;
+let data=inp.value;
+
+inp.value="";
+
+})
+
+// inp.addEventListener("input",function(e){
+
+//     h2.innerText=e.target.value
+// })
